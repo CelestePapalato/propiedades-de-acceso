f8cb3c9203c13fd672b5b08105e3b665bcb7ab23
CelestePapalato <50720541+CelestePapalato@users.noreply.github.com>
2024-04-30 20:49:24 -0300

    Creado cubito

---
+++ Assets/Cubito/Cubo.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubo : MonoBehaviour
+{
+    public float velocidad;
+    private string nombre;
+    public string Nombre
+    {
+        get { return nombre; }
+        set
+        {
+            if (!IsStringEmpty(value))
+            {
+                nombre = value;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Movimiento();
+    }
+
+    void Movimiento()
+    {
+        Vector2 horizontal = new Vector2(Input.GetAxis("Horizontal"), 0);
+        transform.Translate(horizontal * velocidad * Time.deltaTime);
+    }
+
+    bool IsStringEmpty(string text)
+    {
+        foreach(char character in text)
+        {
+            if(character != ' ' && character != '\n')
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
